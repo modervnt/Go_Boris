349c8bd92daabf6a8117da0366ec9794a22d30c0
modervnt <99042355+modervnt@users.noreply.github.com>
2024-12-05 21:21:52 +0300

    main.go

--- main.go
+++ main.go
@@ -1,32 +1,81 @@
 package main
 
 import (
-	"io"
+	"fmt"
+	"image"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"gocv.io/x/gocv"
+)
+
+var (
+	err      error
+	webcam   *gocv.VideoCapture
+	frame_id int
 )
 
 func main() {
-	router := gin.Default()
+	host := "localhost:3000"
 
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Welcome to our video streaming platform!")
-	})
+	// open webcam
+	if len(os.Args) < 2 {
+		fmt.Println(">> device /dev/video0 (default)")
+		webcam, err = gocv.VideoCaptureDevice(0)
+	} else {
+		fmt.Println(">> file/url :: " + os.Args[1])
+		webcam, err = gocv.VideoCaptureFile(os.Args[1])
+	}
 
-	router.GET("/stream/:filename", func(c *gin.Context) {
-		filename := c.Param("filename")
-		file, err := os.Open("videos/" + filename)
-		if err != nil {
-			c.String(http.StatusNotFound, "Video not found.")
+	if err != nil {
+		fmt.Printf("Error opening capture device: \n")
+		return
+	}
+	defer webcam.Close()
+
+	// Servir les fichiers statiques (HTML, CSS, JS)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	// Route pour la vidéo
+	http.HandleFunc("/video", videoHandler)
+
+	fmt.Printf("Server started at http://%s\n", host)
+	log.Fatal(http.ListenAndServe(host, nil))
+}
+
+func videoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "multipart/x-mixed-replace; boundary=frame")
+
+	img := gocv.NewMat()
+	defer img.Close()
+
+	for {
+		if ok := webcam.Read(&img); !ok {
+			fmt.Printf("Device closed\n")
 			return
 		}
-		defer file.Close()
+		if img.Empty() {
+			continue
+		}
 
-		c.Header("Content-Type", "video/mp4")
-		io.Copy(c.Writer, file)
-	})
+		frame_id++
+		gocv.Resize(img, &img, image.Point{}, float64(0.5), float64(0.5), 0)
+		frame, err := gocv.IMEncode(".jpg", img)
+		if err != nil {
+			fmt.Printf("Error encoding frame: %v\n", err)
+			continue
+		}
 
-	router.Run(":8080")
+		fmt.Println("Frame ID: ", frame_id)
+
+		data := "--frame\r\nContent-Type: image/jpeg\r\n\r\n" + string(frame.GetBytes()) + "\r\n"
+		_, err = w.Write([]byte(data))
+		if err != nil {
+			return
+		}
+
+		time.Sleep(33 * time.Millisecond)
+	}
 }
